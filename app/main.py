0aee19c94131bc8c4150950e281bc53cfff9b64e
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-04 18:14:08 +0500

    up2date

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from app!")
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+model = genai.GenerativeModel(model_name="gemini-2.0-flash")
+
+user_input = ("Enter your message: ")
+
+response  = model.generate_content(user_input)
+
+print(response.text)
+
